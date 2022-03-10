b05848dc1284867209d9de85b7ddef55704ea6fa
Loïc Pinçon <lpincon@ippon.Fr>
2022-03-10 16:59:24 +0100

    ajout configuration pour le messaging

--- src/main/java/fr/ippon/epitech/notifications/NotificationsApplication.java
+++ src/main/java/fr/ippon/epitech/notifications/NotificationsApplication.java
@@ -2,10 +2,12 @@ package fr.ippon.epitech.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
+@EnableDiscoveryClient
 public class NotificationsApplication {
 
 	public static void main(String[] args) {
